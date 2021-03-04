e03065f43c51e1419268bd5f6f6b919a42a73902
J <j@Jacobs-MacBook-Pro.local>
2021-03-03 23:50:28 -0800

    set up mongodb connections

---
+++ models/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/example";
+const configOptions = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+};
+
+mongoose.connect(connectionString, configOptions)
+    .then(() => console.log('MongoDB successfully connected...'))
+    .catch(err => console.log('MongoDB connection error:', err));
+
+module.exports = {
+    Expense: require('./expense'),
+};
